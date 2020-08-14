1683f0aff59ad05dd0b68dc707de5253b5bd8467
Gabriel Alves <gabriel.iflasher@gmail.com>
2020-08-14 16:33:31 -0400

    Produtos e associação muitos para muitos

---
+++ src/main/java/com/gabrielalves/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.gabrielalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gabrielalves.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
